d3eb3cba2369c1cabc936a1c52d1b06bdfaa6638
vedashri07 <vedashrimungekar07@gmail.com>
2021-07-02 16:52:46 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,84 @@
+const data ={
+    products :[
+        {
+            _id:'1',
+            name:'Nike Slim shirt',
+            category:'Shirts',
+            image:'/images/NikeShirt.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numRevies:10,
+            description :'high quality'
+
+
+
+        },
+        {
+            _id:'2',
+            name:'Adidas Fit shirt',
+            category:'Shirts',
+            image:'/images/AdidasShirt.jpg',
+            price:100,
+            brand:'Adidas',
+            rating:4.5,
+            numRevies:10,
+            description :'high quality'
+        },
+        {
+            _id:'3',
+            name:'Lacoste Free shirt',
+            category:'Shirts',
+            image:'/images/LacosteShirt.jpg',
+            price:220,
+            brand:'Lacoste',
+            rating:4.8,
+            numRevies:17,
+            description :'high quality'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pants',
+            category:'Shirts',
+            image:'/images/NikePant.jpg',
+            price:320,
+            brand:'Nike',
+            rating:4.5,
+            numRevies:10,
+            description :'high quality'
+
+
+
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pants',
+            category:'Pants',
+            image:'/images/PumaPant.jpg',
+            price:320,
+            brand:'Puma',
+            rating:4.5,
+            numRevies:10,
+            description :'high quality'
+
+
+
+        },
+        {
+            _id:'6',
+            name:'Lacoste Slim Pants',
+            category:'Pants',
+            image:'/images/LacostePant.jpg',
+            price:120,
+            brand:'Lacoste',
+            rating:4.5,
+            numRevies:10,
+            description :'high quality'
+
+
+
+        },
+    ],
+};
+
+export default data;
